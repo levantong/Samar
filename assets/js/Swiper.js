224239b0d22024ed5fc196ed99eda9b4c1305d33
levantong <vantong112.vpb@gmail.com>
2021-09-25 16:36:43 +0700

    Sửa slider + 3 nút dưới bên trái

--- assets/js/Swiper.js
+++ assets/js/Swiper.js
@@ -1,31 +1,30 @@
 var galleryTop = new Swiper('.gallery-top', {
-    spaceBetween: 20,
-    autoplay: {
-      delay: 5000,
-    },
-    loopedSlides: 100,
-    loop: true,
-    speed: 1000,
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-  });
-  var galleryThumbs = new Swiper('.gallery-thumbs', {
-    spaceBetween: 20,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    slideToClickedSlide: true,
-    loopedSlides: 100,
-    loop: true,
-    speed: 1000,
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-  });
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  loop: true,
+  loopedSlides: 100,
+
+  spaceBetween: 10,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  slideToClickedSlide: true,
+});
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 20,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  slideToClickedSlide: true,
+  loop: true,
+  loopedSlides: 100
+});
   
   galleryTop.on('slideChangeTransitionEnd', function() {
     let index_currentSlide = galleryTop.realIndex;
